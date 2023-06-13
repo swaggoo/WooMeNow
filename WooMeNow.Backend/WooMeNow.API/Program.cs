5907550b2972d7d2020c40f4be315645e64ac08d
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-06-13 18:01:47 +0300

    Add Client App

--- WooMeNow.Backend/WooMeNow.API/Program.cs
+++ WooMeNow.Backend/WooMeNow.API/Program.cs
@@ -4,7 +4,7 @@ using WooMeNow.API.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -15,6 +15,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(policyBuilder =>
+{
+    policyBuilder.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("https://localhost:4200");
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
