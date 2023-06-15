1c56ce83632a4959987b3ebd6d5a8ef513a14e7c
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-06-15 15:00:43 +0300

    Downgrade Angular CLI and Update ngx-bootstrap

--- WooMeNow.ClientApp/src/app/app.module.ts
+++ WooMeNow.ClientApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +19,8 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
