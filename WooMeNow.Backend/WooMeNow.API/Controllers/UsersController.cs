ef5394fd34aa8412dfb639012df971c48713b1f3
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-06-27 20:52:45 +0300

    Add the update profile functionality

--- WooMeNow.Backend/WooMeNow.API/Controllers/UsersController.cs
+++ WooMeNow.Backend/WooMeNow.API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using WooMeNow.API.Data;
 using WooMeNow.API.Data.Repository;
 using WooMeNow.API.Models;
@@ -13,10 +14,12 @@ namespace WooMeNow.API.Controllers;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
 
     public UsersController(IUserRepository userRepository, IMapper mapper)
     {
         _userRepository = userRepository;
+        _mapper = mapper;
     }
 
     [HttpGet]
@@ -29,5 +32,19 @@ public class UsersController : BaseApiController
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         return Ok(await _userRepository.GetMemberAsync(username));
+    }
+
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return NotFound();
+
+        _mapper.Map(memberUpdateDto, user);
+
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update user");
     }
 }
